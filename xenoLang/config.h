7118fe6bcfa99477430559bf8c134c697eda840e
VL_PLAY <mr.bonie39@gmail.com>
2025-10-19 16:21:39 +0300

    v0.0.8

---
+++ xenoLang/config.h
@@ -0,0 +1,11 @@
+const char* xeno_language_version = "v0.0.8"
+const char* xeno_language_date = "19.10.2025"
+const char* xeno_language_name = "Xeno Language"
+
+const char* xeno_compiler_name = "Xeno Compiler"
+const char* xeno_compiler_version = "v0.0.8"
+const char* xeno_compiler_date = "19.10.2025"
+
+const char* xeno_vm_name = "Xeno Virtual Machine"
+const char* xeno_vm_version = "v0.0.8"
+const char* xeno_vm_date = "19.10.2025"
