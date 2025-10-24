bb914bbd3623901a635dd6e7618f3b9cc05283be
VL_PLAY <mr.bonie39@gmail.com>
2025-10-24 17:39:48 +0300

    fix config

--- xenoLang/config.h
+++ xenoLang/config.h
@@ -1,11 +1,11 @@
-const char* xeno_language_version = "Pre v0.1.0"
-const char* xeno_language_date = "24.10.2025"
-const char* xeno_language_name = "Xeno Language"
+const char* xeno_language_version = "Pre v0.1.0";
+const char* xeno_language_date = "24.10.2025";
+const char* xeno_language_name = "Xeno Language";
 
-const char* xeno_compiler_name = "Xeno Compiler"
-const char* xeno_compiler_version = "Pre v0.1.0"
-const char* xeno_compiler_date = "24.10.2025"
+const char* xeno_compiler_name = "Xeno Compiler";
+const char* xeno_compiler_version = "Pre v0.1.0";
+const char* xeno_compiler_date = "24.10.2025";
 
-const char* xeno_vm_name = "Xeno Virtual Machine"
-const char* xeno_vm_version = "Pre v0.1.0"
-const char* xeno_vm_date = "24.10.2025"
+const char* xeno_vm_name = "Xeno Virtual Machine";
+const char* xeno_vm_version = "Pre v0.1.0";
+const char* xeno_vm_date = "24.10.2025";
