30d3cbe0abb0dd2e90848ff756d45a14793ec563
VL_PLAY <mr.bonie39@gmail.com>
2025-10-26 11:36:42 +0300

    add license Apache 2.0 for every file

--- xenoLang/core/xeno_vm.h
+++ xenoLang/core/xeno_vm.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2025 VL_PLAY Games
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 #ifndef XENO_VM_H
 #define XENO_VM_H
 
@@ -814,7 +831,7 @@ protected:
     static constexpr const char* xeno_vm_name = "Xeno Virtual Machine";
     static constexpr const char* xeno_vm_version = "v0.1.0";
     static constexpr const char* xeno_vm_date = "26.10.2025";
-    
+
     XenoVM() {
         initializeDispatchTable();
         resetState();
